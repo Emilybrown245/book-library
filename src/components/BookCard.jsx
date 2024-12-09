71681c2c372d223d93dd19127aa090fd48b73dfc
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-12-09 17:05:36 +0000

    adding functionality to components

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,5 +1,17 @@
-function BookCard() {
-  return <p className="border border-primary">Book</p>;
+import Card from 'react-bootstrap/Card'
+
+function BookCard({ book }) {
+ 
+  return (
+    <>
+      <Card>
+    <Card.Body><Card.Title>{book.volumeInfo.title}</Card.Title>
+    <Card.Text>{book.volumeInfo.authors ? <p>By: {book.volumeInfo.authors[0]}</p> : null}</Card.Text>
+    {book.volumeInfo.imageLinks ? (<img src={book.volumeInfo.imageLinks.smallThumbnail}></img>) : null}
+    </Card.Body>
+    </Card>
+    </>
+  )
 }
 
 export default BookCard;
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,17 +1,31 @@
 import BookCard from "./BookCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function BookList({ searchTerm }) {
   const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`)
+.then((response) => {
+return response.json();
+})
+.then(({items}) => {
+  setIsLoading(false);
+setBooks(items) 
+})
+.catch((err) => {
+  setIsLoading(false)
+  setError(true)
+})
+  }, [searchTerm]);
 
   return (
     <div className="border border-success">
       <h2>Book List:</h2>
-      <ul>
-        <BookCard />
-        <BookCard />
-        <BookCard />
-      </ul>
+      {isLoading ? (<p>Your Books are Loading...</p>) : error ? <p>Oh No! Error!!!</p> : <ul>{books.map((book) => {return <BookCard book={book} key={book.id}/>})}</ul>}
     </div>
   );
 }
